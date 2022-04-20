8630355e583f59c4394045956534fe3640cb983c
AkiRusProd <akirusprod@gmail.com>
2022-04-20 22:39:43 +0300

    cuda kernels moved into kernels folder

--- build.py
+++ build.py
@@ -4,7 +4,7 @@ import subprocess
 
 def create_dll(filename):
     """Create DLL file from CUDA (Windows)"""
-    proc = subprocess.Popen(f'nvcc -o dependencies/{filename}.dll  -lcublas -lcurand --shared {filename}.cu')
+    proc = subprocess.Popen(f'nvcc -o dependencies/{filename}.dll  -lcublas -lcurand --shared kernels/{filename}.cu')
     out, err = proc.communicate()
 
 create_dll('mat_mul')
